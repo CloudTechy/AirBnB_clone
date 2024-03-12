8cf804be9d37ef1102294818ff0ee783c1a29bd2
Onyekelu Chukwuebuka <o.enje@rgu.ac.uk>
2024-03-12 09:53:21 +0100

    resolved importing filesorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,8 +3,7 @@
 
 import unittest
 import os
-import json
-from AirBnB_clone.models.engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
 
